115266bb2533e4f6565cacbf33834eb96d2cd786
tranglq <lequoctrang4@MacBook-Pro.local>
2023-12-13 23:15:54 +0700

    done

--- src/screens/AnalyzeScreen.tsx
+++ src/screens/AnalyzeScreen.tsx
@@ -191,6 +191,7 @@ const Analyze = ({ navigation }: { navigation: any }) => {
         if (!token)
           throw new Error("Bạn không có phân quyền truy cập vào dữ liệu!");
         let res = await getBalanceUserInDate(token, selectedDate);
+        console.log(res);
         if (res.error === undefined) throw new Error("Lỗi server");
         if (res.error) throw new Error(res.message);
         let d = new Date(selectedDate);
